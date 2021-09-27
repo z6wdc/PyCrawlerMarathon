9e77bf3f46b7e4370d57c9a05705a77b685c289e
z6wdc <whitedeathcross@gmail.com>
2021-09-27 15:07:05 +0900

    hw25

--- homework/25/demo/spiders/ettoday.py
+++ homework/25/demo/spiders/ettoday.py
@@ -1,10 +1,14 @@
 import scrapy
 
-
 class EttodaySpider(scrapy.Spider):
     name = 'ettoday'
     allowed_domains = ['www.ettoday.net']
     start_urls = ['http://www.ettoday.net/']
 
+    start_urls = ['https://www.ettoday.net/news/20201004/1824032.htm',
+                  'https://www.ettoday.net/news/20201009/1826868.htm']
+
     def parse(self, response):
-        pass
+        print(response.xpath('//title/text()').get())
+        print(response.xpath('//div[@itemprop="articleBody"]//p/text()').getall())
+        
